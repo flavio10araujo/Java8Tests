5f339c25d5a2aa65db6289d3f42d01c4cacaad0a
flavio10araujo <flavio10araujo@yahoo.com.br>
2016-11-11 18:57:36 -0200

    Lambdas

--- src/com/tests/java8/T01Lambas/AMain.java
+++ src/com/tests/java8/T01Lambas/AMain.java
@@ -1,7 +1,9 @@
 package com.tests.java8.T01Lambas;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AMain {
 
@@ -48,6 +50,39 @@ public class AMain {
 		t3.forEach(item -> {
 			if (item.equals("b")) {
 				System.out.println(item);
+			}
+		});
+		
+		
+		System.out.println("### EXEMPLO 03 ###");
+		// Como era pra iterar um Map.
+		Map<String, Integer> items = new HashMap<>();
+		items.put("A", 10);
+		items.put("B", 20);
+		items.put("C", 30);
+		items.put("D", 40);
+		items.put("E", 50);
+		items.put("F", 60);
+
+		for (Map.Entry<String, Integer> entry : items.entrySet()) {
+			System.out.println("Item : " + entry.getKey() + " Count : " + entry.getValue());
+		}
+		
+		// Como é pra iterar um Map com lambda.
+		Map<String, Integer> items2 = new HashMap<>();
+		items2.put("A", 10);
+		items2.put("B", 20);
+		items2.put("C", 30);
+		items2.put("D", 40);
+		items2.put("E", 50);
+		items2.put("F", 60);
+
+		items2.forEach((k, v) -> System.out.println("Item : " + k + " Count : " + v));
+
+		items2.forEach((k, v) -> {
+			System.out.println("Item : " + k + " Count : " + v);
+			if ("E".equals(k)) {
+				System.out.println("Hello E!");
 			}
 		});
 	}
